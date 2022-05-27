1c61a87e27eb19a0ab5067dfbcfdf92abd51d1c2
Voylin <0voylin0@gmail.com>
2022-05-27 19:26:12 +0900

    Blog update

--- run.py
+++ run.py
@@ -2,9 +2,16 @@ import shutil
 import os
 from time import process_time
 
-pages = {}
-
 main_template = ''
+splitter = '#######'
+pages = []
+categories = {
+  'voylins_life': ['voylins_life.html', 'Voylin\'s Life'],
+  'our_life': ['our_life.html', 'Our Life'],
+  'nihongo_de': ['nihongo_de.html', '日本語で'],
+  'learn_japanese': ['learn_japanese.hmtl', 'Learn Japanese'],
+  'travel_advice': ['travel_advice.html', 'Travel Advice'],
+}
 elements = {
   '{{head}}': 'head.html',
   '{{sidebar}}': 'sidebar.html',
@@ -27,11 +34,16 @@ def clear_build():
 
 def prepare_defaults():
   shutil.copyfile('coding-stuff/style.css', 'docs/style.css')
-  os.mkdir('docs/posts')
-  os.mkdir('docs/images')
-  os.mkdir('docs/category')
+  for x in ['posts', 'images']:
+    os.mkdir('docs/{}'.format(x))
   for _image in os.listdir('coding-stuff/images'):
     shutil.copyfile('coding-stuff/images/' + _image, 'docs/images/' + _image)
+
+def load_pages():
+  global pages
+  for x in os.listdir('coding-stuff/posts'): pages.append('posts/{}'.format(x))
+  for x in os.listdir('coding-stuff/main_pages'): pages.append('main_pages/{}'.format(x))
+  for x in os.listdir('coding-stuff/blog_categories'): pages.append('blog_categories/{}'.format(x))
 
 
 def create_main_template():
@@ -46,91 +58,69 @@ def create_main_template():
     main_template = main_template.replace(key, element_string)
     element_data.close()
 
-def get_breadcrumb(type, page):
-  print("Creating breadcrumb for type '{}' and page '{}'".format(type, page))
+def get_breadcrumb(type, page, category = ''):
   breadcrumb = '<a href="index.html"><svg width="12" height="12" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M575.8 255.5C575.8 273.5 560.8 287.6 543.8 287.6H511.8L512.5 447.7C512.5 450.5 512.3 453.1 512 455.8V472C512 494.1 494.1 512 472 512H456C454.9 512 453.8 511.1 452.7 511.9C451.3 511.1 449.9 512 448.5 512H392C369.9 512 352 494.1 352 472V384C352 366.3 337.7 352 320 352H256C238.3 352 224 366.3 224 384V472C224 494.1 206.1 512 184 512H128.1C126.6 512 125.1 511.9 123.6 511.8C122.4 511.9 121.2 512 120 512H104C81.91 512 64 494.1 64 472V360C64 359.1 64.03 358.1 64.09 357.2V287.6H32.05C14.02 287.6 0 273.5 0 255.5C0 246.5 3.004 238.5 10.01 231.5L266.4 8.016C273.4 1.002 281.4 0 288.4 0C295.4 0 303.4 2.004 309.5 7.014L564.8 231.5C572.8 238.5 576.9 246.5 575.8 255.5L575.8 255.5z"/></svg></a>/'
-  if type == 'main_pages':
+  if type in ['main_pages', 'blog_categories']:
     if page != 'index.html':
-      breadcrumb += '<a href="{}">{}</a>'.format(page, page[:-5])
+      breadcrumb += '<a href="{}">{}</a>'.format(page, page[:-5].replace('_', ' ').title())
+  elif type == 'post':
+    breadcrumb += '<a href="{}">{}</a>/<a href="{}">{}</a>'.format(categories[category][0], categories[category][1].title(), page, page[:-5].replace('_', ' ').title())
+
   return breadcrumb
 
-def load_posts():
-  # Load all posts and get their main categories and sub categories
-  print("Still need to load posts...")
 
-  for x in os.listdir('coding-stuff/posts'):
-    post_path = 'posts/' + x
-    post_file = open('coding-stuff/' + post_path)
-    post_data = post_file.read()
-    post_file.close()
-
-    pages['docs/' + post_path] = post_data
-
-
-def load_pages():
-  for x in os.listdir('coding-stuff/main_pages'):
-    _origin = 'coding-stuff/main_pages/' + x
-    _path = 'docs/' + x
+def create_pages():
+  global pages
+  for x in pages:
     page = main_template
-  
-    page_file = open(_origin, 'r')
-    page_data = page_file.read().split('#######')
-    page_file.close()
-  
-    page_info = page_data[0].splitlines()
-    page_content = page_data[1]
-    page = page.replace('{{page_title_text}}', _page_title(page_info))
-    page = page.replace('{{content}}', page_content)
-    breadcrumb = get_breadcrumb('main_pages', x)
-    page = page.replace('{{breadcrumbs}}', breadcrumb)
 
-    if x == '404.html':
-      page = '---\npermalink: /404.html\n---\n' + page
+    _origin = 'coding-stuff/{}'.format(x)
+    _folder = x.split('/')[0]
+    _filename = x.split('/')[1]
+    _path = 'docs/' + _filename
+    
+    __file__ = open(_origin, 'r')
+    _raw_page = __file__.read()
+    __file__.close()
+
+    page_info = {}
+    page_content = 'empty'
+    if splitter in _raw_page:
+      for line in _raw_page.split(splitter)[0].split('\n'):
+        if line != '':
+          page_info[line.split(': ')[0]] = line.split(': ')[1]
+      page_content = _raw_page.split(splitter)[1]
+    else:
+      print('ERROR for page: {}, no \'{}\'!'.format(x, splitter))
+      page_content = _raw_page
+    
+
+    if 'page_title' in page_info.keys():
+      page = page.replace('{{page_title_text}}', page_info['page_title'])
+    else:
+      print('ERROR: Page {} has no title!')
+    
+    page = page.replace('{{content}}', page_content)
+
+    if _folder == 'posts':
+      if 'category' in page_info.keys():
+        page = page.replace('{{breadcrumbs}}', get_breadcrumb(_folder, _filename, page_info['category']))
+      else: print('ERROR: Post {} has no category!')
+    else:
+      page = page.replace('{{breadcrumbs}}', get_breadcrumb(_folder, _filename))
+
+    if _filename == '404.html': page = '---\npermalink: /404.html\n---\n' + page
 
     new_page = open(_path, 'w')
     new_page.write(page)
     new_page.close()
-
-
-def load_blog_category_pages():
-  for x in os.listdir('coding-stuff/blog_categories'):
-    _origin = 'coding-stuff/blog_categories/' + x
-    _path = 'docs/category/' + x
-    page = main_template
-  
-    page_file = open(_origin, 'r')
-    page_data = page_file.read().split('#######')
-    page_file.close()
-  
-    page_info = page_data[0].splitlines()
-    page_content = page_data[1]
-    page = page.replace('{{page_title_text}}', _page_title(page_info))
-    page = page.replace('{{content}}', page_content)
-    breadcrumb = get_breadcrumb('main_pages', x)
-    page = page.replace('{{breadcrumbs}}', breadcrumb)
-
-    page = page.replace('href="style.css"', 'href="../style.css"')
-    page = page.replace('src="images', 'src="../images')
-
-    new_page = open(_path, 'w')
-    new_page.write(page)
-    new_page.close()
-
-
-def _page_title(page_info):
-  page_title_line = 'page_title: '
-  for line in page_info:
-    if page_title_line in line:
-      return line[page_title_line.__len__():]
-  return '{{page_title_text}}'
 
 
 def prepare_site():
   clear_build()
   prepare_defaults()
-  create_main_template()
-  load_posts()
   load_pages()
-  load_blog_category_pages()
+  create_main_template()
+  create_pages()
 
 prepare_site() 
