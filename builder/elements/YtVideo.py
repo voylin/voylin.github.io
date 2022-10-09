f76fb7b92f8d60be2cfbf98339a6f6c83bb6fc42
Voylin <0voylin0@gmail.com>
2022-10-09 16:12:08 +0900

    Removal old stuff

--- builder/elements/YtVideo.py
+++
@@ -1,8 +0,0 @@
-def get_yt_video(vid_id):
-  return f"""
-  <div class="yt-container">
-  <iframe class="yt-video" src="https://www.youtube.com/embed/{vid_id}?rel=0" frameBorder="0">
-    Browser does not support embedded videos.
-  </iframe>
-</div>
-"""
